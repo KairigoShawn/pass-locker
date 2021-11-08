f632f56cf91f29d9aaa5979e7e597bb7cfc5e39f
KairigoShawn <kairigonjogu@outlook.com>
2021-11-08 14:51:04 +0300

    creates function to allow creation of a new user

--- passmanager.py
+++ passmanager.py
@@ -2,3 +2,10 @@
 import pyperclip
 from user_credentials import User, Credential
 
+def create_user(fname,lname,password):
+	'''
+	allows us to create a new user
+	'''
+	new_user = User(fname,lname,password)
+	return new_user
+
