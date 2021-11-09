83539f70e3e629518d4e7d2c2895da7e9c332456
KairigoShawn <kairigonjogu@outlook.com>
2021-11-09 10:21:27 +0300

    adds functions in the user class that defines properties and saves new users info

--- user_credentials.py
+++ user_credentials.py
@@ -9,3 +9,20 @@ class User:
 	allows usto create new users and save their information for future usage
 	'''
 
+	users_list = []
+	def __init__(self,first_name,last_name,password):
+		'''
+		defining properties
+		'''
+
+		# In variables
+		self.first_name = first_name
+		self.last_name = last_name
+		self.password = password
+
+	def save_user(self):
+		'''
+		saves new user created
+		'''
+		User.users_list.append(self)
+
