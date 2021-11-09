95e7b2658ffec8f14f19e9b475b01ae4fde7669c
KairigoShawn <kairigonjogu@outlook.com>
2021-11-09 10:46:51 +0300

    saves new user credentials

--- user_credentials.py
+++ user_credentials.py
@@ -55,3 +55,10 @@ class Credential:
 		self.account_name = account_name
 		self.password = password
 
+	def save_credentials(self):
+		'''
+		Function to save a newly created user instance
+		'''
+
+		Credential.credentials_list.append(self)
+
